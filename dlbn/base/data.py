f5db9f509f8be90cde761d5930c3fd3aa50a2eb5
Howard <934303306@qq.com>
2021-06-26 15:56:52 +0800

    Data类的调用

--- dlbn/base/data.py
+++ dlbn/base/data.py
@@ -27,9 +27,34 @@ class Data:
         states = sorted(list(self.DataFrame.loc[:, variable].unique()))
         return states
 
+    def _state_count(self, **kwargs):
+        """
+        状态计数器，用于计算特定的条件下的样本数
+        parameters:
+        for example (A = 1, B = 2)
+        """
+        query = kwargs
+        count = 0
+        for row_tuple in self.DataFrame.iterrows():
+            series = row_tuple[1]
+            flag = True
+            for _ in query.items():
+                if series[_[0]] == _[1]:
+                    continue
+                else:
+                    flag = False
+                    break
+            if flag:
+                count += 1
+
+        return count
+
+    def contingency_table(self, **kwargs):
+        pass
+
 
 if __name__ == '__main__':
     data = pd.read_excel(r"../test/test_data.xlsx")
     a = Data(data)
     print(a.variables)
-    print(a.state_names)
+    print(a._state_count(A=1, B=0))
