5eb459308be66c8c92dfdd6a79da5cc2ef7ab3d1
HUANG HAO <934303306@qq.com>
2021-06-28 14:26:03 +0800

    BIC local score function

--- dlbn/estimator/score_function.py
+++ dlbn/estimator/score_function.py
@@ -35,12 +35,19 @@ class BicScore(Score):
         self.contingency_table = Data(contingency_table)
 
     def local_score(self, child: str, parents: list):
-        values = []
+        likelihood = 0
         for index, series in self.contingency_table.DataFrame.iterrows():
             parents_config = series.loc[parents].to_dict()
-            values.append(series['count'] * np.log(
-                series['count'] / self.contingency_table.state_count_in_conTB(**parents_config)))
-        return np.sum(values)
+            _ = series['count'] * np.log(
+                series['count'] / self.contingency_table.state_count_in_conTB(**parents_config))
+            likelihood += _
+        # *******************************
+        sample_size = self.contingency_table.DataFrame['count'].sum()
+        num_parents_states = len(self.contingency_table.collect_state_names(parents))
+        num_child_states = len(self.contingency_table.state_names[child])
+        penalization = 0.5 * np.log(sample_size) * num_parents_states * (num_child_states - 1)
+
+        return likelihood-penalization
 
 
 if __name__ == '__main__':
